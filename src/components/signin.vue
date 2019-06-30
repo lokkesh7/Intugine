c044cd365976495b49c6a34d8d4dd6aa5d2ae5c4
Lokkesh Krishna Ramakrishnan <lramakrishnan@outlook.com>
2019-07-01 00:03:20 +0530

    Signin Page

---
+++ src/components/signin.vue
@@ -0,0 +1,104 @@
+<template>
+  <div id="signin">
+    <div class="signin-form">
+      <form @submit.prevent="onSubmit">
+        <div class="input">
+          <label for="username">Mail</label>
+          <input
+                  type="text"
+                  id="username"
+                  v-model="username">
+        </div>
+        <div class="input">
+          <label for="password">Password</label>
+          <input
+                  type="password"
+                  id="password"
+                  v-model="password">
+        </div>
+        <div class="submit">
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        username: '',
+        password: ''
+      }
+    },
+    methods: {
+      onSubmit () {
+        const userData = {
+          username: this.username,
+          password: this.password
+        }
+        // Dispatching action to login with user date from actions in store.js if checkbox is checked
+        this.$store.dispatch('login', userData)
+        
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .signin-form {
+    width: 400px;
+    margin: 30px auto;
+    border: 1px solid #eee;
+    padding: 20px;
+    box-shadow: 0 2px 3px #ccc;
+  }
+
+  .input {
+    margin: 10px auto;
+  }
+
+  .input label {
+    display: block;
+    color: #4e4e4e;
+    margin-bottom: 6px;
+  }
+
+  .input input {
+    font: inherit;
+    width: 100%;
+    padding: 6px 12px;
+    box-sizing: border-box;
+    border: 1px solid #ccc;
+  }
+
+  .input input:focus {
+    outline: none;
+    border: 1px solid #521751;
+    background-color: #eee;
+  }
+
+  .submit button {
+    border: 1px solid #521751;
+    color: #521751;
+    padding: 10px 20px;
+    font: inherit;
+    cursor: pointer;
+  }
+
+  .submit button:hover,
+  .submit button:active {
+    background-color: #521751;
+    color: white;
+  }
+
+  .submit button[disabled],
+  .submit button[disabled]:hover,
+  .submit button[disabled]:active {
+    border: 1px solid #ccc;
+    background-color: transparent;
+    color: #ccc;
+    cursor: not-allowed;
+  }
+</style>
