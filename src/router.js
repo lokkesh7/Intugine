f73f01fa555e4d481cf5348ee099799f96ce426c
Lokkesh Krishna Ramakrishnan <lramakrishnan@outlook.com>
2019-07-01 00:08:07 +0530

    Vue Router import

---
+++ src/router.js
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+import WelcomePage from './components/welcome.vue'
+import SigninPage from './components/signin.vue'
+import Dashboard from '@/components/dashboard.vue'
+import store from '@/store/store'
+//using the vue router in vue application
+Vue.use(VueRouter)
+
+const routes = [
+  { path: '/', component: WelcomePage },
+  { path: '/signin', component: SigninPage },
+  { path: '/dashboard', component: Dashboard,
+  // This is navigation gaurd used to protect the route. It will be redirected only if the token is available
+  //next() is important. If next() is missing, then the route will not redirected
+    beforeEnter (to,from,next) {
+      if (store.state.token) {
+        next()
+      } else {
+        next('/signin')
+      }
+    }
+  }
+]
+
+export default new VueRouter({mode: 'history', routes})
