03814546ce6976d4d50c223a8506bde83f45062f
Lokkesh Krishna Ramakrishnan <lramakrishnan@outlook.com>
2019-07-02 23:25:59 +0530

    Component Updation

---
+++ src/components/dashboard.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="dashboard">
+  	<div class="row">
+    <!--imported google maps component-->
+	  <div class="col-10"><Gmaps /></div>
+    <!--imported Device component-->
+	  <div class="col-2"><Devices /></div>
+	</div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Gmaps from '@/components/Gmaps.vue'
+import Devices from '@/components/Devices.vue'
+
+export default {
+  name: 'dashboard',
+  components: {
+    Gmaps,
+    Devices
+  }
+}
+</script>
