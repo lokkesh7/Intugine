0f349fa6c4dd2dd44bd2025669e52ec654d1fbdc
Lokkesh Krishna Ramakrishnan <lramakrishnan@outlook.com>
2019-07-02 23:26:42 +0530

    Axios request interceptors added

---
+++ src/utils/axios-auth.js
@@ -0,0 +1,22 @@
+import axios from 'axios'
+
+//Created custom axios instance for all get request with bearer token
+const instance = axios.create({
+  baseURL: 'https://dl5opah3vc.execute-api.ap-south-1.amazonaws.com'
+})
+//let token = localStorage.getItem('token')
+//instance.defaults.headers.get['Authorization'] = `Bearer ${token}`
+
+instance.interceptors.request.use(
+  (config) => {
+    let token = localStorage.getItem('token');
+
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${ token }`;
+    }
+
+    return config;
+  }
+)
+
+export default instance
